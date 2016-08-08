0ea10cad7e7254e90a52dd927261c9a6ee0f5bd2
InvasiveLionfish <InvasiveLionfish@users.noreply.github.com>
2016-08-08 15:36:26 -0700

    Implemented Sockets.IO on server and client

--- app-server.js
+++ app-server.js
@@ -1,9 +1,16 @@
 var express = require('express');
-
 var app = express();
 
 app.use(express.static('./public'));
 app.use(express.static('./node_modules/bootstrap/dist'));
 
-app.listen(3000);
+var server = app.listen(3000);
+var io = require('socket.io').listen(server);
+
+//creating an event listener for the sockets connection
+io.sockets.on('connection', function(socket) {
+  console.log("Connected: %s", socket.id);
+});
+
+
 console.log("Polling server is running at 'http://localhost:3000'");
