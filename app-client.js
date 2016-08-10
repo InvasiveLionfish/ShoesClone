77ca541b66d5cd472bfe716fb03b3595623dc192
InvasiveLionfish <InvasiveLionfish@users.noreply.github.com>
2016-08-09 17:17:38 -0700

    Created routes but broke Router

--- app-client.js
+++ app-client.js
@@ -1,5 +1,32 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var APP = require('./components/APP');
+var ReactRouter = require('react-router');
+var Router = ReactRouter.Router;
+var Route = ReactRouter.Route;
+var DefaultRoute = Router.DefaultRoute;
 
-ReactDOM.render(<APP />, document.getElementById('react-container'));
+var APP = require('./components/APP');
+var Audience = require('./components/Audience');
+var Speaker = require('./components/Speaker');
+var Board = require('./components/Board');
+
+var routes = (
+    <Route component={APP}>
+        <DefaultRoute component={Audience} />
+        <Route name="speaker" path="speaker" component={Speaker}></Route>
+        <Route name="board" path="board" component={Board}></Route>
+    </Route>
+  );
+
+// Router.run(routes, function(Handler){
+//   ReactDOM.render(<Handler />, document.getElementById('react-container'));
+// });
+
+ReactDOM.render(<Router>{routes}</Router>, document.getElementById('react-container'));
+
+
+
+
+
+
+//So later we may change the var routes since we want to use the new 2shoes front-page, the default is set to audience right now. 
---
+++ components/Board.js
@@ -0,0 +1,11 @@
+var React = require('React');
+
+var Board = React.createClass({
+  render() {
+    return (
+        <h1>Board</h1>
+      );
+  }
+});
+
+module.exports = Board; 
